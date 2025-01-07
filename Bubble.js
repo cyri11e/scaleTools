862f3d7c1208803e85e487c2476de74324ded510
Cyrille Labbé <cyrille.labbe@equasens.com>
2025-01-07 13:54:50 +0100

    effet glow

--- Bubble.js
+++ Bubble.js
@@ -7,13 +7,15 @@ class Bubble {
         this.currentLabelIndex = 0; // Index du label actuel
         this.hue = hue;
         this.isInScale = isInScale;
-        this.visualMode = visualMode;
+        this.visualMode = 'invisible'; // Mode visuel par défaut
         this.labelType = labelType; // Ajouter la propriété labelType
     }
 
     draw() {
         push();
         colorMode(HSB, 12); // Utiliser le mode HSB       
+        let shadowOffset = this.radius / 20;
+        textAlign(CENTER, CENTER);
 
         if (this.visualMode === 'color') {
             if (this.isInScale) {
@@ -29,10 +31,11 @@ class Bubble {
             colorMode(RGB); // Revenir au mode RVB
             noStroke();
             textSize(this.radius * 1.3)
-            textAlign(CENTER, CENTER);
-            let shadowOffset = this.radius / 20;
+
+
             fill(50);
             text(this.labels[this.labelType] || '', this.x + shadowOffset, this.y + shadowOffset); // Utiliser labelType pour choisir le label
+            
             if ( this.isInScale ) {
                 fill(255);  
             } else {    
@@ -42,9 +45,31 @@ class Bubble {
             text(this.labels[this.labelType] || '', this.x - shadowOffset, this.y - shadowOffset); // Utiliser labelType pour choisir le label
             noFill();
         } else if (this.visualMode === 'invisible') {
+            // effet glow
+                  // Ajouter un effet de glow
+            drawingContext.shadowBlur = 10;
+            drawingContext.shadowColor = 'rgba(255, 255, 255, 0.7)';
+   
+            if (this.isMouseOver()) {
+                strokeWeight(3); // Augmenter l'épaisseur du contour si la bulle est survolée
+            }          
+
             noFill();
             stroke(255);
             ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+            
+            noStroke()
+            if ( this.isInScale ) { 
+                fill(255)
+                textSize(this.radius * 1.3)
+                text(this.labels[this.labelType] || '', this.x + shadowOffset, this.y + shadowOffset); // Utiliser labelType pour choisir le label
+            } else {
+                
+                fill(0, 0, 12, this.isMouseOver() ? 12 : 0.5)
+                textSize(this.radius )
+
+                text(this.labels[this.labelType] || '', this.x + shadowOffset, this.y + shadowOffset); // Utiliser labelType pour choisir le label
+            }        
         }
         pop();
     }
